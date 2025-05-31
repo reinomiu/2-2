45d2503254eae3aa9d2ad7f5fd59716845770e5b
reinomiu <fl25a006@oecu.jp>
2025-05-31 11:14:10 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(randint(0, 10))
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showNumber(randint(0, 10))
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showNumber(randint(0, 10))
+})
+basic.forever(function () {
+	
+})
